b39d127d53684350ce6ac893a012b5c0f3584aff
ABO3MIM11 <aboodzxzx11@gmail.com_GitHub>
2025-11-18 21:49:27 +0300

    Update server.js for Railway

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
-const server = require("http").createServer(app);
-const WebSocket = require("ws");
+const PORT = process.env.PORT || 8080;
 
-// WebSocket server
-const wss = new WebSocket.Server({ server });
-
-// استخدم PORT اللي Railway أو أي خدمة استضافة ترسله
-const PORT = process.env.PORT || 3000;
-
-// تقديم الملفات الثابتة (index.html والملفات الأخرى)
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// افتح السيرفر
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
